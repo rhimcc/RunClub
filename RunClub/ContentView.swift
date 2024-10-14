cd707b114de5df1eac3cb76c09354e28fc795b49
rhimcc <rio.mccormack25@gmail.com>
2024-10-14 22:49:47 +1100

    Sign Up/In Views, not linked with actual authentication

--- RunClub/ContentView.swift
+++ RunClub/ContentView.swift
@@ -9,53 +9,42 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        NavigationStack {
+            Spacer()
+            Text("Run Club")
+                .font(.system(size: 50))
+                .bold()
+                
+            Spacer()
+            
+            NavigationLink {
+                SignInView()
+            } label : {
+                Text("SIGN IN")
+                    .bold()
+            }.buttonStyle(.borderedProminent)
+                .padding(.vertical)
+                .tint(.black)
+
+            NavigationLink {
+                SignUpView()
+            } label : {
+                Text("SIGN UP")
+                    .bold()
+            }.buttonStyle(.borderedProminent)
+                .tint(.black)
+
+            Spacer()
+            Spacer()
+            
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+  
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
---
+++ RunClub/SignInView.swift
@@ -0,0 +1,73 @@
+//
+//  SignInView.swift
+//  RunClub
+//
+//  Created by Rhianna McCormack on 14/10/2024.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @Environment(\.dismiss) var dismiss
+    @State private var showSignUp: Bool = false
+
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Sign In")
+                .font(.title)
+                .bold()
+            Spacer()
+            VStack {
+                HStack {
+                    VStack (alignment: .trailing){
+                        Text("Email")
+                            .bold()
+                            .padding(.bottom)
+                        Text("Password")
+                            .bold()
+                    }
+                    VStack {
+                        TextField("Email", text: $email)
+                            .textFieldStyle(.roundedBorder)
+                        TextField("Password", text: $password)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+                
+            }
+            Button {
+                print("check sign in")
+            } label : {
+                Text("SIGN IN")
+                    .bold()
+            }.buttonStyle(.borderedProminent)
+                .tint(.black)
+                .padding(20)
+                .disabled(email.isEmpty || password.isEmpty)
+            
+            HStack {
+                Text("Dont have an account?")
+                NavigationLink {
+                    SignUpView()
+                } label: {
+                    Text("Create an account")
+                        .underline()
+                        .foregroundStyle(.black)
+                    }
+                }
+            
+            Spacer()
+            Spacer()
+            
+            
+        }.padding(.horizontal, 20)
+        
+    }
+}
+
+#Preview {
+    SignInView()
+}
