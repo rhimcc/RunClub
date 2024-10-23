1724fe48a9fb1fc0f48e0b386c7d1f3627b3dd5e
rhimcc <rio.mccormack25@gmail.com>
2024-10-23 18:43:48 +1100

    Creating friends view

---
+++ RunClub/Views/FriendsView.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsView.swift
+//  RunClub
+//
+//  Created by Rhianna McCormack on 23/10/2024.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Friends")
+                    .font(.title)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    FriendsView()
+}
