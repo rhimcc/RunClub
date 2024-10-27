ad49558416b4e1440f9e3e77667bc16b9ba5817b
rhimcc <rio.mccormack25@gmail.com>
2024-10-27 17:32:15 +1100

    RunSummary Page

--- RunClub/Views/RecordRun/RunSummaryView.swift
+++ RunClub/Views/RecordRun/RunSummaryView.swift
@@ -11,30 +11,63 @@ struct RunSummaryView: View {
     let firestore = FirestoreService()
     var locationManager: LocationService
     var run: Run
+    @State var clubs: [Club] = []
+    @State var events: [Event] = []
+    @State var selectedClub: Club? = nil
+    @State var selectedEvent: Event? = nil
+    
+
     var body: some View {
         VStack {
             Text("Your Run")
                 .font(.title)
-                .padding(.top, 20)
             let size = UIScreen.main.bounds.width - 40
             RouteMapView(showUserLocation: false, locationManager: locationManager)
                 .frame(width: size, height: size)
             RunMetricsView(locationManager: locationManager, buttonShown: false)
             
             HStack {
-                Text("Club")
+                Text("Club:")
                 Spacer()
-                Text("club")
-                
+                Picker("Clubs", selection: $selectedClub) {
+                    ForEach(clubs, id: \.self) { club in
+                        Text(club.name)
+                            .foregroundStyle(Color.white)
+                            .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
+                    }
+                }
             }
             HStack {
                 Text("Event")
                 Spacer()
-                Text("Event")
+                Picker("Events", selection: $selectedEvent) {
+                    ForEach(events.filter { $0.clubId == selectedClub?.id }, id: \.id) { event in
+                         Text(event.name)
+                             .foregroundStyle(Color.white)
+                             .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
+                     }
+                }
             }
         }.padding(20)
 
         .navigationBarBackButtonHidden(true)
+        .onAppear {
+            firestore.getUsersClubs(userId: User.getCurrentUserId()) { clubs, error in
+                DispatchQueue.main.async {
+                    if let clubs = clubs  {
+                        self.clubs = clubs
+                    }
+                }
+            }
+            firestore.getClubsUserOwns(userId: User.getCurrentUserId()) { clubs, error in
+                DispatchQueue.main.async {
+                    if let clubs = clubs  {
+                        self.clubs = self.clubs + clubs
+                    }
+                }
+            }
+            
+        }
     }
 }
 
